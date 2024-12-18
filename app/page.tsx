85a2d01e1ef9699dae90c3a60b7ea57b0391ab17
Muhammad Tahir Hassan <mtahirhassan826@gmail.com>
2024-12-18 16:03:18 +0500

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,126 @@
-import Image from "next/image";
-
-export default function Home() {
+import logo from "@/public/11.png"
+import Image from "next/image"
+import landingPic from "@/public/0.png"
+export default function page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main>
+      <div className="flex w-full bg-orange-100 px-10 py-10 flex-col">
+        <div className="flex w-full h-16 border border-gray-600 rounded-full  items-center justify-between">
+          <div className="ml-4">
+            <Image className="w-24" src={logo} alt="logo"></Image>
+          </div>
+          <div className="flex gap-5">
+            <p className="text-gray-600">HOME</p>
+            <p className="text-gray-600">RECIPES</p>
+            <p className="text-gray-600">COOKING TIPS</p>
+            <p className="text-gray-600">ABOUT US</p>
+          </div>
+          <div className="flex mr-4">
+            <p className="flex bg-black p-2 text-white rounded-full">Subsucribe</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className="flex w-full bg-orange-100 px-10 py-10 justify-center  border border-gray-600 rounded-3xl mt-4">
+          <div className="flex justify-center items-center flex-col">
+            <p className="text-white bg-red-600 px-3 py-1 rounded-full">RECIPE</p>
+            <p className="flex text-center text-7xl font-bold">LEMON GARLIC<br />ROASTED CHICKEN</p>
+            <p className="w-96 mt-10">Welcome to Cooks Delight, where culinary dreams come alive! Today, we embark on a journey of flavors with a dish that promises to elevate your dining experience – our Citrus Infusion Delight: Lemon Garlic Roasted Chicken</p>
+            <div className="flex mt-10 gap-10">
+              <p className="font-bold text-xl">1 HOUR</p>
+              <p className="font-bold text-xl">HARD PREP</p>
+              <p className="font-bold text-xl">4 SERVES</p>
+            </div>
+            <div className="mt-10">
+              <Image src={landingPic} alt="landing pic"></Image>
+            </div>
+            <div className="flex w-full mt-10">
+              <div className="flex-col w-full mt-10">
+                <div>
+                  <p className="float-left max-w-xl ml-16" >Picture succulent chicken infused with the bright notes of lemon and the
+                    aromatic richness of garlic. It's a symphony of flavors that will have your taste
+                    buds dancing. Join us as we delve into the art of roasting and uncover the
+                    secrets behind creating a masterpiece that's not just a meal but a celebration
+                    of culinary craftsmanship.<br /><br />As you preheat your oven, envision the kitchen filling with the tantalizing
+                    aroma of herbs and citrus, setting the stage for a delightful meal that
+                    transcends the ordinary. The anticipation builds as the chicken roasts to
+                    golden perfection, promising a dining experience that marries simplicity with
+                    sophistication. Whether you're a seasoned chef or a kitchen novice, this recipe
+                    invites you to become a culinary artist, creating a masterpiece that will leave a
+                    lasting impression on your guests and loved ones.</p>
+                </div>
+              </div>
+              <div className="w-96 h-80 flex-col justify-center items-center bg-white rounded-3xl mt-10 border border-gray-300 p-5">
+                <h2 className="text-red-500 text-xl">INGREDIENTS</h2>
+                <div>
+                  <ul>
+                    <li><span className="text-2xl font-bold"> .</span> 1 whole chicken (about 3 4 poundsi)</li>
+                    <li><span className="text-2xl font-bold"> .</span> 2 lemons, slicej</li>
+                    <li><span className="text-2xl font-bold"> .</span> 6 cloves garlic, mincej</li>
+                    <li><span className="text-2xl font-bold"> .</span> 2 tablespoons olive oi|</li>
+                    <li><span className="text-2xl font-bold"> .</span> 1 teaspoon dried thymx</li>
+                    <li><span className="text-2xl font-bold"> .</span> 1 teaspoon dried rosemar</li>
+                    <li><span className="text-2xl font-bold"> .</span> Salt and black pepper to taste</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="flex mt-10">
+              <div>
+                <p className="text-black font-bold text-2xl">Let’s go over the basics– the do’s, and the<br />
+                  don’ts– for How to Cook a chicken</p>
+                <p className="text-red-500 font-bold mt-3 text-xl">DO'S:</p>
+                <p className="font-bold mt-4 text-xl text-gray-950">Thoroughly Clean Hands and Surfaces: </p>
+                <p className="text-gray-700">Before and after handling raw chicken, ensure your hands, utensils, and<br />
+                  surfaces are clean to prevent cross-contamination.</p>
+                <p className="font-bold mt-4 text-xl text-gray-950">Use Separate Cutting Boards: </p>
+                <p className="text-gray-700">Dedicate specific cutting boards for raw chicken to avoid the spread of<br />
+                  bacteria to other foods.</p>
+                <p className="font-bold mt-4 text-xl text-gray-950">Check Internal Temperature: </p>
+                <p className="text-gray-700">Invest in a reliable meat thermometer to ensure the chicken reaches the safe<br />
+                  internal temperature of 165°F (74°C).</p>
+                <p className="text-red-500 font-bold mt-3 text-xl">DONT'S:</p>
+                <p className="font-bold mt-4 text-xl text-gray-950">Thaw Chicken at Room Temperature: </p>
+                <p className="text-gray-700">Avoid thawing chicken on the counter. Instead, thaw it in the refrigerator to<br />
+                  prevent bacterial growth.</p>
+                <p className="font-bold mt-4 text-xl text-gray-950">Overcrowd the Pan:  </p>
+                <p className="text-gray-700">When roasting, ensure the chicken pieces have space between them for even<br />
+                  cooking. Overcrowding can lead to unevenly cooked chicken</p>
+              </div>
+              <div>
+                <div className="w-96 h-56 flex-col justify-center items-center bg-white rounded-3xl mt-10 border border-gray-300 p-5 ml-72">
+                  <h2 className="text-red-500 text-xl">Equipment Needed for<br />
+                    Preparation</h2>
+                  <div>
+                    <ul>
+                      <li><span className="text-2xl font-bold"> .</span> Roasting pan</li>
+                      <li><span className="text-2xl font-bold"> .</span> Meat thermometer</li>
+                      <li><span className="text-2xl font-bold"> .</span> 6 cloves garlic, mincej</li>
+                      <li><span className="text-2xl font-bold"> .</span> Kitchen twine</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="w-96 h-52 flex-col justify-center items-center bg-white rounded-3xl mt-10 border border-gray-300 p-5 ml-72">
+                  <h2 className="text-red-500 text-xl">Nutritional Value</h2>
+                  <p>Per serving (based on a 4-pound chicken):</p>
+                  <div>
+                    <ul>
+                      <li>Calories: ~250</li>
+                      <li>Protein: ~30g</li>
+                      <li>Total Fat: ~13g</li>
+                      <li>Carbohydrates: ~5g</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div className="justify-start">
+                <h1>Instructions</h1>
+              </div>
+          </div>
+          
+        </div>
+
+      </div>
+    </main >
+  )
 }
